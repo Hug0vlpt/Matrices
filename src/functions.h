7ae9f631c1ec30eb65f63397990316093d5e853e
hugo <4hugo23@gmail.com>
2023-06-27 00:04:34 +0200

    update function.h

---
+++ src/functions.h
@@ -0,0 +1,13 @@
+#ifndef __FUNCTIONS_H_
+#define _FUNCTIONS_H_
+
+Matrix SUM(Matrix A, Matrix B);
+Matrix Product(Matrix A, Matrix B);
+Matrix Transpose(Matrix A);
+Matrix Invert(Matrix A);
+
+int Get_trace(Matrix A);
+int Get_rank(Matrix A);
+
+#endif__FUNCTIONS_H_
+#define _FUNCTIONS_H_
