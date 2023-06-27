c8ea75457a3230f45a7198150c785207207031d2
hugo <4hugo23@gmail.com>
2023-06-27 23:28:32 +0200

    update user

---
+++ src/user.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include "user.h"
+#include "struct.h"
+#include "functions.h"
+
+void display_options()
+{
+  int option;
+  do{
+    printf("Which action do you want to do ? Sum (1)/ : ");
+    scanf("%d",&option);
+    clearBuffer();
+    if (option<0 || option>1){
+      printf("Please enter a number between 1 and 9\n");
+    }
+  } while(option<0 || option>1);
+
+  switch(option){
+    case 1: Sum(); break;        
+  }
+}
+
+void clearBuffer() {
+  char c;
+  while ((c = getchar()) != '\n' && c != EOF);
+}
+
+void Size(Matrix* A)
+{
+  do{
+    printf("The number of rows of the matrix: ");
+    scanf("%d",&A->nb_r);
+    if (A->nb_r <1){
+      printf("The number of rows must be upper or equal to 1");
+    }
+    clearBuffer();
+  }while (A->nb_r <1);
+  do{
+    printf("The number of columns of the matrix: ");
+    scanf("%d",&A->nb_c);
+    if (A->nb_c <1){
+      printf("The number of columns must be upper or equal to 1");
+    }
+    clearBuffer();
+  }while (A->nb_c <1);
+}
+
+void enterValues(Matrix* A)
+{
+  for (int i=0; i<A->nb_r; ++i){
+    for (int j=0; j<A->nb_c; ++j){
+      printf("Enter the value of the row n°%d and the column n°%d: ",i+1,j+1);
+      scanf("%d",&A->arr[i][j]);
+    }
+  }
+}
+
+void display_Matrix(Matrix A)
+{
+  for (int i=0; i<A.nb_r; ++i){
+    printf("  | ");
+    for (int j=0; j<A.nb_c; ++j){
+      printf("%d | ",A.arr[i][j]);
+    }
+    printf("\n");
+  }
+}
---
+++ src/user.h
@@ -0,0 +1,12 @@
+#ifndef __OUTPUT_H_
+#define __OUTPUT_H_
+#include "struct.h"
+
+void display_options();
+void Size(Matrix* A);
+void enterValues(Matrix* A);
+void clearBuffer();
+void display_Matrix(Matrix A);
+void display_Info_Matrix(Matrix A);
+
+#endif
