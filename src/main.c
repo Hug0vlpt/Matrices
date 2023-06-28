ad5203c8b887d645d785d2bab62352fe064e5f70
hugo <4hugo23@gmail.com>
2023-06-28 14:59:31 +0200

    system clear when restart

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "user.h"
 
@@ -11,6 +12,7 @@ int main (int argc, char *argv[])
     printf("Do you want to do something else ? (y/n): ");
     clearBuffer();
     fgets(token, 2, stdin);
+    system("clear");
   } while(!strcmp(token,"y"));
 
   return 0;
