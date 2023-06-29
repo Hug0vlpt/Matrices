c4b9f31c1a99d27aa6732409bda7cc0a2b294bb6
hugo <4hugo23@gmail.com>
2023-06-29 21:49:13 +0200

    Matrix A parameters removed

--- src/is_functions.h
+++ src/is_functions.h
@@ -1,21 +1,20 @@
 #ifndef __ISFUNCTIONS_H_
 #define __ISFUNCTIONS_H_
-#include "struct.h"
 
-int is_Row(Matrix A);
-int is_Column(Matrix A);
-int is_Null(Matrix A);
-int is_Square(Matrix A);
-int is_Identity(Matrix A);
-int is_Elementary(Matrix A);
-int is_Invertible(Matrix A);
-int is_Diagonal(Matrix A);
-int is_Scalar(Matrix A);
-int is_Upper_Triangle(Matrix A);
-int is_Strictly_Upper_Triangle(Matrix A);
-int is_Lower_Triangle(Matrix A);
-int is_Strictly_Lower_Triangle(Matrix A);
-int is_Symmetric(Matrix A);
-int is_AntiSymmetric(Matrix A);
+void is_Row();
+void is_Column();
+void is_Null();
+void is_Square();
+void is_Identity();
+void is_Elementary();
+void is_Invertible();
+void is_Diagonal();
+void is_Scalar();
+void is_Upper_Triangle();
+void is_Strictly_Upper_Triangle();
+void is_Lower_Triangle();
+void is_Strictly_Lower_Triangle();
+void is_Symmetric();
+void is_AntiSymmetric();
 
 #endif
