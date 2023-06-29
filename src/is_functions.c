2fca981df0fdc07d75966322751538c40ef84aee
hugo <4hugo23@gmail.com>
2023-06-29 21:48:50 +0200

    new function is_Row

---
+++ src/is_functions.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "struct.h"
+#include "user.h"
+#include "functions.h"
+
+void is_Row()
+{
+  Matrix* A = malloc(sizeof(Matrix));
+  Size(A);
+  printf("\n");
+
+  if (A->nb_r == 1){
+    printf("The matrix is a row one.\n\n");
+    A->is_row = 1;
+  } else {
+    printf("The matrix isn't a row one.\n\n");
+    A->is_row = 0;
+  }
+}
