c3ca25d440c687b4bd472a66e01e0c78d8ac0809
hugo <4hugo23@gmail.com>
2023-06-30 16:13:30 +0200

    update functions and comments

--- src/is_functions.h
+++ src/is_functions.h
@@ -4,17 +4,43 @@
 void is_Row();
 void is_Column();
 void is_Null();
+
+//Square check
 void is_Square();
+void is_SquareM(Matrix* A);
+
+//Identity check
 void is_Identity();
+void is_IdentityM(Matrix* A);
+
+//Elementary check
 void is_Elementary();
+
+//Invertible check
 void is_Invertible();
+
+//Diagonal check
 void is_Diagonal();
+
+//Scalar check
 void is_Scalar();
+
+//Upper Triangle check
 void is_Upper_Triangle();
+
+//Stictly Upper Triangle check
 void is_Strictly_Upper_Triangle();
+
+//Lower triangle check
 void is_Lower_Triangle();
+
+//Strictly Lower Triangle check
 void is_Strictly_Lower_Triangle();
+
+//Symmetric check
 void is_Symmetric();
+
+//Antisymmetric check
 void is_AntiSymmetric();
 
 #endif
